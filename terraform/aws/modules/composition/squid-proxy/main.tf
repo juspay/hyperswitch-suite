4560e29d4581ec68e355113820523c7b2d1d99b7
Shailesh-714 <shaileshwaran714@gmail.com>
2026-01-08 19:39:27 +0530

    update iam

--- terraform/aws/modules/composition/squid-proxy/main.tf
+++ terraform/aws/modules/composition/squid-proxy/main.tf
@@ -130,8 +130,8 @@ module "squid_iam_role" {
   count  = var.create_iam_role ? 1 : 0
   source = "../../base/iam-role"
 
-  name                    = "${var.environment}-${var.project_name}-squid-role"
-  description             = "IAM role for Squid proxy instances"
+  name                    = "${local.name_prefix}-role"
+  description             = "IAM role for ${local.name_prefix} proxy instances"
   service_identifiers     = ["ec2.amazonaws.com"]
   create_instance_profile = true
 
@@ -232,7 +232,7 @@ module "asg_security_group" {
   source = "../../base/security-group"
 
   name        = "${local.name_prefix}-asg-sg"
-  description = "Security group for Squid proxy ASG instances"
+  description = "Security group for ${local.name_prefix} proxy ASG instances"
   vpc_id      = var.vpc_id
 
 
@@ -355,7 +355,7 @@ module "launch_template" {
   source = "../../base/launch-template"
 
   name               = local.name_prefix
-  description        = "Launch template for Squid proxy instances"
+  description        = "Launch template for ${local.name_prefix} proxy instances"
   ami_id             = var.ami_id
   instance_type      = var.instance_type
   key_name           = var.generate_ssh_key ? aws_key_pair.squid_key_pair[0].key_name : var.key_name
