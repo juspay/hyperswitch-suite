14c4c2dc17b250280d375893c1c3acd53065aa07
venkatesh.devendran <venkatesh.devendran@juspay.in>
2026-01-10 16:25:05 +0530

    fix output files

--- terraform/aws/live/dev/eu-central-1/security-rules/outputs.tf
+++ terraform/aws/live/dev/eu-central-1/security-rules/outputs.tf
@@ -2,34 +2,14 @@
 # Security Rules Outputs
 # ============================================================================
 
-output "locker_ingress_rule_ids" {
-  description = "IDs of locker ingress security group rules"
-  value       = module.security_rules.locker_ingress_rule_ids
+output "ingress_rule_ids" {
+  description = "Map of ingress security group rule IDs grouped by security group ID"
+  value       = module.security_rules.ingress_rule_ids
 }
 
-output "locker_egress_rule_ids" {
-  description = "IDs of locker egress security group rules"
-  value       = module.security_rules.locker_egress_rule_ids
-}
-
-output "nlb_ingress_rule_ids" {
-  description = "IDs of NLB ingress security group rules"
-  value       = module.security_rules.nlb_ingress_rule_ids
-}
-
-output "nlb_egress_rule_ids" {
-  description = "IDs of NLB egress security group rules"
-  value       = module.security_rules.nlb_egress_rule_ids
-}
-
-output "squid_ingress_rule_ids" {
-  description = "IDs of squid ingress security group rules"
-  value       = module.security_rules.squid_ingress_rule_ids
-}
-
-output "squid_egress_rule_ids" {
-  description = "IDs of squid egress security group rules"
-  value       = module.security_rules.squid_egress_rule_ids
+output "egress_rule_ids" {
+  description = "Map of egress security group rule IDs grouped by security group ID"
+  value       = module.security_rules.egress_rule_ids
 }
 
 output "rules_summary" {
