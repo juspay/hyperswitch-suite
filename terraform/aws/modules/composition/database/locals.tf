986c9a6c6bc961ba732e21e0a8da4474afaf88e6
Shailesh <shaileshwaran.s@juspay.in>
2026-02-10 12:37:50 +0530

    fix warning

--- terraform/aws/modules/composition/database/locals.tf
+++ terraform/aws/modules/composition/database/locals.tf
@@ -11,7 +11,7 @@ locals {
     var.tags
   )
 
-  region                     = var.region != null ? var.region : data.aws_region.current.name
+  region                     = var.region != null ? var.region : data.aws_region.current.region
   cluster_identifier         = var.cluster_identifier != null ? var.cluster_identifier : "${local.name_prefix}-cluster"
   db_subnet_group_name       = var.db_subnet_group_name != null ? var.db_subnet_group_name : "${local.name_prefix}-subnet-group"
   security_group_name        = var.security_group_name != null ? var.security_group_name : "${local.name_prefix}-sg"
