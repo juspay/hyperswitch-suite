94d0f116e0483b6a83c5807c005a7984012a871d
Harshvardhan Bahukhandi <harshvardhan.b@juspay.in>
2026-02-12 12:00:29 +0530

    chore: removed encryption-service and eso

--- terraform/aws/live/dev/eu-central-1/apps/eso-encryption-service/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "hyperswitch-dev-terraform-state"
-    key     = "dev/eu-central-1/apps/encryption-service/terraform.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
-  }
-}
