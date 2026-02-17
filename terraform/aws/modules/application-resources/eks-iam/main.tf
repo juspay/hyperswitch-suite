42046cc70ae5b14d89f9cfd1c17a06604899a61a
Harshvardhan Bahukhandi <harshvardhan.b@juspay.in>
2026-02-17 15:06:50 +0530

    fix: removed duplicated module from the main.tf in eks-iam application resource module

--- terraform/aws/modules/application-resources/eks-iam/main.tf
+++ terraform/aws/modules/application-resources/eks-iam/main.tf
@@ -105,17 +105,4 @@ resource "aws_iam_role_policy" "inline" {
   policy = each.value
 }
 
-output "role_name" {
-  description = "Name of the created IAM role"
-  value       = aws_iam_role.this.name
-}
 
-output "role_arn" {
-  description = "ARN of the created IAM role"
-  value       = aws_iam_role.this.arn
-}
-
-output "role_id" {
-  description = "ID of the created IAM role"
-  value       = aws_iam_role.this.id
-}
