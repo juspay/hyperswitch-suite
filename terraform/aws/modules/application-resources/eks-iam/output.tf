3d57dd77ca4dd69f6f235891608644fe683ca36b
Harshvardhan Bahukhandi <90124977+itsharshvb@users.noreply.github.com>
2026-02-17 18:49:19 +0530

    fix: corrected value refrence for output.tf in eks-iam application resource module (#100)

--- terraform/aws/modules/application-resources/eks-iam/output.tf
+++ terraform/aws/modules/application-resources/eks-iam/output.tf
@@ -1,14 +1,14 @@
 output "role_name" {
   description = "Name of the created IAM role"
-  value       = module.eks_iam.role_name
+  value       = aws_iam_role.this.name
 }
 
 output "role_arn" {
   description = "ARN of the created IAM role"
-  value       = module.eks_iam.role_arn
+  value       = aws_iam_role.this.name
 }
 
 output "role_id" {
   description = "ID of the created IAM role"
-  value       = module.eks_iam.role_id
+  value       = aws_iam_role.this.name
 }
