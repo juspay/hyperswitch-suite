25bca8574f6ac3637cbd893343a947b87a80580a
Harshvardhan Bahukhandi <harshvardhan.b@juspay.in>
2026-03-25 17:01:07 +0530

    chore: add db creation support to grafana module

--- terraform/aws/modules/application-resources/grafana/outputs.tf
+++ terraform/aws/modules/application-resources/grafana/outputs.tf
@@ -51,3 +51,62 @@ output "customer_managed_policies_enabled" {
   description = "Whether customer managed policy attachments feature is enabled"
   value       = local.customer_managed_policies_enabled
 }
+
+output "database_enabled" {
+  description = "Whether database feature is enabled"
+  value       = local.database_enabled
+}
+
+# =========================================================================
+# DATABASE OUTPUTS
+# =========================================================================
+output "database_cluster_id" {
+  description = "RDS Cluster Identifier (if database is created)"
+  value       = var.create_database ? module.database[0].cluster_id : null
+}
+
+output "database_cluster_arn" {
+  description = "ARN of the RDS cluster (if database is created)"
+  value       = var.create_database ? module.database[0].cluster_arn : null
+}
+
+output "database_endpoint" {
+  description = "Writer endpoint for the database (if database is created)"
+  value       = var.create_database ? module.database[0].endpoint : null
+}
+
+output "database_reader_endpoint" {
+  description = "Reader endpoint for the database (if database is created)"
+  value       = var.create_database ? module.database[0].reader_endpoint : null
+}
+
+output "database_name" {
+  description = "Name of the database (if database is created)"
+  value       = var.create_database ? module.database[0].database_name : null
+}
+
+output "database_port" {
+  description = "Port for the database (if database is created)"
+  value       = var.create_database ? module.database[0].port : null
+}
+
+output "database_master_username" {
+  description = "Master username for the database (if database is created)"
+  value       = var.create_database ? module.database[0].master_username : null
+  sensitive   = true
+}
+
+output "database_security_group_id" {
+  description = "ID of the security group for the database (if created)"
+  value       = var.create_database ? module.database[0].security_group_id : null
+}
+
+output "database_cluster_instance_ids" {
+  description = "Map of cluster instance identifiers (if database is created)"
+  value       = var.create_database ? module.database[0].cluster_instance_ids : null
+}
+
+output "database_cluster_instance_endpoints" {
+  description = "Map of cluster instance endpoints (if database is created)"
+  value       = var.create_database ? module.database[0].cluster_instance_endpoints : null
+}
