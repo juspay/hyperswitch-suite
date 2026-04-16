6b8c3007390502eef4d6035ce27033c7cec72fd0
Pradesh <pradesh.s@juspay.in>
2026-04-16 20:14:50 +0530

    remove the attributes from the outputs.tf

--- terraform/aws/modules/composition/efs/outputs.tf
+++ terraform/aws/modules/composition/efs/outputs.tf
@@ -14,21 +14,6 @@ output "file_system_dns_names" {
   value       = { for k, v in module.efs : k => v.dns_name }
 }
 
-output "file_system_names" {
-  description = "Map of file system keys to names"
-  value       = { for k, v in module.efs : k => v.name }
-}
-
-output "file_system_size_in_bytes" {
-  description = "Map of file system keys to size in bytes"
-  value       = { for k, v in module.efs : k => v.size_in_bytes }
-}
-
-output "file_system_number_of_mount_targets" {
-  description = "Map of file system keys to number of mount targets"
-  value       = { for k, v in module.efs : k => v.number_of_mount_targets }
-}
-
 # EFS Mount Target Outputs
 output "mount_target_ids" {
   description = "Map of mount target keys to IDs"
@@ -98,40 +83,6 @@ output "access_point_file_system_arns" {
     for k, v in module.efs : k => {
       for ap_key, ap in v.access_points : ap_key => ap.file_system_arn
     }
-  }
-}
-
-# EFS Backup Policy Outputs
-output "backup_policy_ids" {
-  description = "Map of file system keys to backup policy IDs"
-  value       = { for k, v in module.efs : k => v.backup_policy_id if v.backup_policy_id != null }
-}
-
-# EFS File System Policy Outputs
-output "file_system_policy_ids" {
-  description = "Map of file system keys to policy IDs"
-  value       = { for k, v in module.efs : k => v.policy_id if v.policy_id != null }
-}
-
-# EFS Replication Configuration Outputs
-output "replication_configuration_ids" {
-  description = "Map of file system keys to replication configuration IDs"
-  value       = { for k, v in module.efs : k => v.replication_configuration_id if v.replication_configuration_id != null }
-}
-
-output "replication_source_file_system_ids" {
-  description = "Map of file system keys to source file system IDs in replication"
-  value = {
-    for k, v in module.efs : k => v.id
-    if try(v.replication_configuration_id, null) != null
-  }
-}
-
-output "replication_creation_times" {
-  description = "Map of file system keys to replication creation times"
-  value = {
-    for k, v in module.efs : k => v.replication_configuration_creation_time
-    if try(v.replication_configuration_creation_time, null) != null
   }
 }
 
