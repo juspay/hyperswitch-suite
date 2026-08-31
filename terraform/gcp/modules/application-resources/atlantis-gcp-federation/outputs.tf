a460fa6ddc8fa6f2a11315303b22908ca6457689
Harshvardhan Bahukhandi <harshvardhan.b@juspay.in>
2026-08-31 19:45:09 +0530

    chore

--- terraform/gcp/modules/application-resources/atlantis-gcp-federation/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "workload_identity_pool_name" {
-  description = "Fully qualified workload identity pool name (projects/.../locations/global/workloadIdentityPools/atlantis-aws-pool)"
-  value       = google_iam_workload_identity_pool.atlantis.name
-}
-
-output "workload_identity_pool_provider_name" {
-  description = "Fully qualified provider name - the resource passed to `gcloud iam workload-identity-pools create-cred-config`"
-  value       = google_iam_workload_identity_pool_provider.atlantis_aws.name
-}
